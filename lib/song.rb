91e01922694661e402e20120689981490271c61d
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-30 21:39:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,5 @@
 class Song
   
-  attr_accessor :artist, :name 
   
-  @@all = []
-  
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def artist_name(name = nil)
-    self.artist.name
-  end
   
 end
