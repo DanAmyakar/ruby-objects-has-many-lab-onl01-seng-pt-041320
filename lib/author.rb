fd1a2933828cf064c352cd712eb212d2eb34bd4d
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-01 00:03:31 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,13 +13,13 @@ class Author
   
   def add_post(post)
     post.author = self
-    @posts << post
+    self.posts << post
   end
   
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
-    @posts << post
+    self.posts << post
   end
   
   def self.post_count
